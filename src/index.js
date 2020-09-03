a6df6292846ee2a8fc3df1e122e5783b854be54f
Louis Dalligos <louis.dalligos@nuworks.ph>
2020-09-03 12:45:07 +0800

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Vendors
+import "./vendor/jquery";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "popper.js/dist/popper.min.js";
+
+import "@scss/main.scss";
+
+$(document).ready(function () {
+  console.log("Test jquery");
+});
