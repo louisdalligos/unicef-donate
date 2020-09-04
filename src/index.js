6cb997efbcbde8d3df693aabe7021bdad0dc398b
Louis Dalligos <louis.dalligos@nuworks.ph>
2020-09-05 02:03:41 +0800

    Home adjust

--- src/index.js
+++ src/index.js
@@ -9,4 +9,19 @@ import "@scss/main.scss";
 
 $(document).ready(function () {
   console.log("Test jquery");
+
+  $("#arrow-down, #cta-donate").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top + 8,
+        },
+        800
+      );
+    }
+  });
 });
